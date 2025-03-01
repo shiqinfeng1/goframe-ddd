13c7773d1c364348463520e8792b10b03f6b7648
shiqinfeng <150627601@qq.com>
2025-03-01 13:27:46 +0800

    🐞 fix: 自动tag失败检查

--- scripts/ensure_tag.sh
+++ scripts/ensure_tag.sh
@@ -2,8 +2,18 @@
 
 version="${VERSION}"
 if [ "${version}" == "" ];then
-  version=v`gsemver bump`
+  version=v`gsemver bump 2>/dev/null`
 fi
+
+# 定义语义化版本号的正则表达式
+semver_pattern='^v[0-9]+\.[0-9]+\.[0-9]+$'
+if [[ $version =~ $semver_pattern ]]; then
+    echo "version 有效，值为: $version"
+else
+    echo "version 无效，不符合语义化版本号格式: ${version}"
+    exit 1
+fi
+
 if [ -z "`git tag -l ${version}`" ];then
   git tag -a -m "release version ${version}" ${version}
 fi
