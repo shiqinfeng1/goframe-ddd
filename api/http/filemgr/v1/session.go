5e247b5685a1ab3a61ae5fb5981ca1baf9a87f94
shiqinfeng <150627601@qq.com>
2025-03-05 08:38:46 +0800

    fix: 🐞 修复会话列表接口定义错误

--- api/http/filemgr/v1/session.go
+++ api/http/filemgr/v1/session.go
@@ -3,7 +3,7 @@ package v1
 import "github.com/gogf/gf/v2/frame/g"
 
 type SessionListReq struct {
-	g.Meta `path:"/file/resume" tags:"会话管理" method:"post" summary:"服务端查询已连接客户端会话列表"`
+	g.Meta `path:"/session/list" tags:"会话管理" method:"post" summary:"服务端查询已连接客户端会话列表"`
 }
 type SessionListRes struct {
 	g.Meta    `mime:"application/json"`
