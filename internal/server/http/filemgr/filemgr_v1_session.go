b088b801cce77d7fc7f4b7446c22c04da17c3cd6
shiqinfeng <150627601@qq.com>
2025-03-05 11:46:19 +0800

    fix: 🐞 编译错误

--- internal/server/http/filemgr/filemgr_v1_session.go
+++ internal/server/http/filemgr/filemgr_v1_session.go
@@ -6,7 +6,7 @@ import (
 	v1 "github.com/shiqinfeng1/goframe-ddd/api/http/filemgr/v1"
 )
 
-func (c *ControllerV1) SessionList(ctx context.Context, req *v1.NodeListReq) (res *v1.NodeListRes, err error) {
+func (c *ControllerV1) NodeList(ctx context.Context, req *v1.NodeListReq) (res *v1.NodeListRes, err error) {
 	out, err := c.app.Queries.GetClientIds(ctx)
 	res = &v1.NodeListRes{
 		NodeIds: out,
