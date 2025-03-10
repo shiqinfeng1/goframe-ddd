f3d79ae32f728f2366a0f9223e856e88c922b1d8
shiqinfeng <150627601@qq.com>
2025-03-10 11:43:02 +0800

    fix: 🐞 修复无效遍历任务列表

--- internal/domain/filemgr/filesendq.go
+++ internal/domain/filemgr/filesendq.go
@@ -5,6 +5,8 @@ import (
 	"sync"
 
 	"github.com/gogf/gf/v2/container/gmap"
+	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/os/gctx"
 )
 
 // FileTransferMgr 表示文件发送队列
@@ -13,7 +15,7 @@ type FileTransferMgr struct {
 	maxTasks int
 	tasks    gmap.StrAnyMap
 	mutex    sync.Mutex
-	cond     *sync.Cond
+	notify   chan struct{}
 	stream   StreamIntf
 	repo     Repository
 }
@@ -28,8 +30,8 @@ func NewFileTransferService(maxTasks int, stm StreamIntf, repo Repository) *File
 		repo:     repo,
 	}
 
-	q.cond = sync.NewCond(&q.mutex)
-	q.start()
+	q.notify = make(chan struct{}, 16) // 运行多个任务通知缓存到通道里
+	q.start(gctx.New())
 	return q
 }
 
@@ -39,16 +41,25 @@ func (q *FileTransferMgr) AddTask(ctx context.Context, id, name, nodeId string, 
 	defer q.mutex.Unlock()
 	task := NewTransferTask(ctx, id, name, nodeId, paths, q.stream, q.repo)
 	q.tasks.Set(id, task)
-	q.cond.Signal()
+	q.notify <- struct{}{}
 }
 
 // Start 开始处理队列中的任务
-func (q *FileTransferMgr) start() {
+func (q *FileTransferMgr) start(ctx context.Context) {
 	go func() {
+		var oldTask *TransferTask
 		for {
+			<-q.notify // 等待通知
 			q.mutex.Lock()
-			for q.running >= q.maxTasks || q.tasks.Size() == 0 {
-				q.cond.Wait() // 等待被通知，同时unlock q.mutex， 等到通知后，会重新lock
+			for q.running >= q.maxTasks {
+				g.Log().Infof(ctx, "%v task is running, waitting...", q.running)
+				q.mutex.Unlock()
+				continue
+			}
+			for q.tasks.Size() == 0 {
+				g.Log().Infof(ctx, "no task in queue!")
+				q.mutex.Unlock()
+				continue
 			}
 			// 找到第一个等待发送的任务
 			var needRemove string
@@ -60,7 +71,6 @@ func (q *FileTransferMgr) start() {
 					q.running++
 					task.sendFileChan <- func(success bool) {
 						q.mutex.Lock()
-						defer q.mutex.Unlock()
 						if success {
 							task.status = StatusSuccessful
 							needRemove = k
@@ -68,56 +78,74 @@ func (q *FileTransferMgr) start() {
 							task.status = StatusFailed
 						}
 						q.running--
-						q.cond.Signal()
+						q.mutex.Unlock()
+						g.Log().Debugf(ctx, "task:%v(%v) finished with %v!", task.taskId, task.taskName, task.status)
+						q.notify <- struct{}{}
 					}
 					return false
 				}
 				return true
 			})
+			// 任务执行完成，从队列中移除
 			if needRemove != "" {
-				q.tasks.Remove(needRemove)
+				old := q.tasks.Remove(needRemove)
+				oldTask = old.(*TransferTask)
 			}
 			q.mutex.Unlock()
+			if needRemove != "" {
+				g.Log().Infof(ctx, "task:%v(%v) do success. remove it!", oldTask.taskId, oldTask.taskName)
+			}
 		}
 	}()
 }
 
 // PauseTask 暂停指定 ID 的任务
 func (q *FileTransferMgr) PauseTask(ctx context.Context, id string) {
+	var (
+		task      *TransferTask
+		oldStatus Status
+	)
+
 	q.mutex.Lock()
-	defer q.mutex.Unlock()
 	q.tasks.Iterator(func(k string, v any) bool {
-		task := v.(*TransferTask)
+		task = v.(*TransferTask)
 		if task.taskId == id &&
 			(task.status == StatusSending || task.status == StatusWaiting) {
+			oldStatus = task.status
 			task.pauseChan <- func() {
 				q.mutex.Lock()
 				task.status = StatusPaused
 				q.running--
-				q.cond.Signal()
 				q.mutex.Unlock()
 			}
 			return false
 		}
 		return true
 	})
+	q.mutex.Unlock()
+	g.Log().Debugf(ctx, "task:%v(%v) change status  %v -> %v !", task.taskId, task.taskName, oldStatus, task.status)
+	q.notify <- struct{}{}
 }
 
 // CancelTask 取消指定 ID 的任务
 func (q *FileTransferMgr) CancelTask(ctx context.Context, id string) {
-	var needRemove bool
+	var (
+		needRemove bool
+		task       *TransferTask
+		oldStatus  Status
+	)
+
 	q.mutex.Lock()
-	defer q.mutex.Unlock()
 	q.tasks.Iterator(func(k string, v any) bool {
-		task := v.(*TransferTask)
+		task = v.(*TransferTask)
 		if task.taskId == id &&
 			(task.status == StatusSending || task.status == StatusWaiting || task.status == StatusPaused) {
+			oldStatus = task.status
 			task.status = StatusPaused
 			task.cancelChan <- func() {
 				q.mutex.Lock()
 				task.status = StatusCancel
 				q.running--
-				q.cond.Signal()
 				q.mutex.Unlock()
 			}
 			needRemove = true
@@ -128,21 +156,34 @@ func (q *FileTransferMgr) CancelTask(ctx context.Context, id string) {
 	if needRemove {
 		q.tasks.Remove(id)
 	}
+	q.mutex.Unlock()
+	if needRemove {
+		g.Log().Debugf(ctx, "task:%v(%v) change status  %v -> %v !", task.taskId, task.taskName, oldStatus, task.status)
+		q.notify <- struct{}{}
+	}
 }
 
 // ResumeTask 恢复指定 ID 的任务
 func (q *FileTransferMgr) ResumeTask(ctx context.Context, id string) {
+	var (
+		found bool
+		task  *TransferTask
+	)
 	q.mutex.Lock()
-	defer q.mutex.Unlock()
 	q.tasks.Iterator(func(k string, v any) bool {
-		task := v.(*TransferTask)
+		task = v.(*TransferTask)
 		if task.taskId == id && task.status == StatusPaused {
 			task.status = StatusWaiting
-			q.cond.Signal()
+			found = true
 			return false
 		}
 		return true
 	})
+	q.mutex.Unlock()
+	if found {
+		g.Log().Debugf(ctx, "task:%v(%v) change status %v -> %v !", task.taskId, task.taskName, StatusPaused, task.status)
+		q.notify <- struct{}{}
+	}
 }
 
 // GetTaskStatus 获取指定 ID 任务的状态
